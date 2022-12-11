37037b7d51d384e153bd89a8761e50a2b10e3e5e
Isu Shin <isu010614@naver.com>
2022-12-12 03:21:33 +0900

    feat: 상품을 추가하는 기능

--- src/main/java/vendingmachine/domain/ProductRepository.java
+++ src/main/java/vendingmachine/domain/ProductRepository.java
@@ -3,17 +3,26 @@ package vendingmachine.domain;
 import vendingmachine.constant.ErrorMessage;
 import vendingmachine.constant.ProductsFormatConstant;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class ProductRepository {
     private static final List<Product> products = new ArrayList<>();
 
-    private static void addProducts(String inputProducts) {
+    public static void addProducts(String inputProducts) {
         String[] eachProduct = inputProducts.split(ProductsFormatConstant.PRODUCT_SEPARATOR);
+        Arrays.stream(eachProduct)
+                .forEach(productInfo -> {
+                    Product product = generateEachProduct(productInfo);
+                    products.add(product);
+                });
+    }
 
+    private static Product generateEachProduct(String product) {
+        String[] productInfo = product.split(ProductsFormatConstant.PRODUCT_COMPONENT_SEPARATOR);
+        String productName = productInfo[0];
+        int productPrice = Integer.parseInt(productInfo[1]);
+        int productQuantity = Integer.parseInt(productInfo[2]);
+        return new Product(productName, productPrice, productQuantity);
     }
 
     public Product findByName(String productName) {
