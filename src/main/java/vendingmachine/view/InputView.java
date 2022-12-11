49f0b1c8206d1d9b5c61211d86142d426e5f6c43
Isu Shin <isu010614@naver.com>
2022-12-11 23:18:59 +0900

    feat: 추가할 상품을 입력받는 기능

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -8,6 +8,14 @@ public class InputView {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         String vendingMachineMoney = Console.readLine();
         InputValidator.validateToInteger(vendingMachineMoney);
+        System.out.println();
         return Integer.parseInt(vendingMachineMoney);
+    }
+
+    public static String inputProducts(){
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+        String products = Console.readLine();
+        System.out.println();
+        return products;
     }
 }
