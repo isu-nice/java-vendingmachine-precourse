de2aafb00d75041308ae4ca12d764bcce9c1dcf7
Isu Shin <isu010614@naver.com>
2022-12-12 03:08:11 +0900

    feat: 잔액을 출력하는 기능

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -4,11 +4,16 @@ import java.util.Map;
 
 public class OutputView {
     public static final String COIN_AND_NUMBER = "%d원 - %d개\n";
+    public static final String INSERT_MONEY = "투입금액: %d원";
 
     public static void printMachineCoins(Map<Integer, Integer> numberOfCoins) {
         System.out.println("자판기가 보유한 동전");
         numberOfCoins.forEach((key, value) -> {
             System.out.printf(COIN_AND_NUMBER, key, value);
         });
+    }
+
+    public static void printInsertMoney(int balance) {
+        System.out.printf(INSERT_MONEY, balance);
     }
 }
