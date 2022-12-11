2a1ee42e19f1afdb095cb03a27d4a3ad9cdb514d
Isu Shin <isu010614@naver.com>
2022-12-11 18:13:09 +0900

    feat: 에러메시지 클래스 추가

---
+++ src/main/java/vendingmachine/constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package vendingmachine.constant;
+
+public class ErrorMessage {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String NOT_NUMERIC = ERROR_PREFIX + "금액은 숫자여야 합니다.";
+
+}
