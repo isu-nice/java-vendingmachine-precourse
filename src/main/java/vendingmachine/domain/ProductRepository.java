1ca64f07e7e45afeadd4da50fe8b2563079a0ec6
Isu Shin <isu010614@naver.com>
2022-12-12 03:01:43 +0900

    feat: 상품 최소값 찾는 기능

--- src/main/java/vendingmachine/domain/ProductRepository.java
+++ src/main/java/vendingmachine/domain/ProductRepository.java
@@ -6,6 +6,7 @@ import vendingmachine.constant.ProductsFormatConstant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class ProductRepository {
     private static final List<Product> products = new ArrayList<>();
@@ -15,12 +16,18 @@ public class ProductRepository {
 
     }
 
-
     public Product findByName(String productName) {
         return products.stream()
                 .filter(product -> product.isSameName(productName))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.NOT_EXIST_PRODUCT_NAME));
+    }
+
+    public static int findMinimumPrice() {
+        return products.stream()
+                .mapToInt(Product::getPrice)
+                .min()
+                .orElseThrow(NoSuchElementException::new);
     }
 
     public static List<Product> products() {
