63a5e9d81148dfe93d592bd53bf9e7caf3d59e62
Isu Shin <isu010614@naver.com>
2022-12-12 01:06:36 +0900

    feat: 상품 입력형식 예외처리 기능

--- src/main/java/vendingmachine/view/InputValidator.java
+++ src/main/java/vendingmachine/view/InputValidator.java
@@ -1,14 +1,26 @@
 package vendingmachine.view;
 
 import vendingmachine.constant.ErrorMessage;
+import vendingmachine.constant.ProductsFormatConstant;
+
+import java.util.Arrays;
 
 public class InputValidator {
+    private static final String PRODUCT_BUNDLE_SYMBOL_REGEX = "[^[a-zA-Zㄱ-ㅎ가-힣0-9]*$]";
 
-    public static void validateToInteger(String input){
-        try{
+    public static void validateToInteger(String input) {
+        try {
             Integer.parseInt(input);
-        }catch (IllegalArgumentException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.NOT_NUMERIC);
         }
+    }
+
+    public static void validateProductsInputFormat(String input) {
+        String[] products = input.split(ProductsFormatConstant.PRODUCT_SEPARATOR);
+        Arrays.stream(products)
+                .filter(product -> product.matches(PRODUCT_BUNDLE_SYMBOL_REGEX))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.INVALID_PRODUCT_FORMAT));
     }
 }
